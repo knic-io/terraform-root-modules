ab9d94fceb071858767bbdfa87de3bc1f1dadd90
Josh Myers <joshuajmyers@gmail.com>
2019-02-07 11:21:39 +0000

    Add k8s admin + read only access roles
    This commit adds admin and readonly roles for Kubernetes.
    aws-iam-authenticator will map these roles internally. Note that these
    roles do not require any policies for access to AWS resource.

--- aws/kops-aws-platform/iam-authenticator.tf
+++ aws/kops-aws-platform/iam-authenticator.tf
@@ -5,12 +5,7 @@ variable "cluster_id" {
 
 variable "kube_config_path" {
   type        = "string"
-  description = "Path to the kube config file. Can be sourced from `KUBE_CONFIG` or `KUBECONFIG`"
-}
-
-variable "admin_iam_role_arn" {
-  type        = "string"
-  description = "IAM Role with admin permissions to map to `admin_k8s_username`"
+  description = "Path to the kube config file"
 }
 
 variable "admin_k8s_username" {
@@ -25,11 +20,6 @@ variable "admin_k8s_groups" {
   default     = []
 }
 
-variable "readonly_iam_role_arn" {
-  type        = "string"
-  description = "IAM Role with readonly permissions to map to `readonly_k8s_username`"
-}
-
 variable "readonly_k8s_username" {
   type        = "string"
   description = "Kubernetes readonly username to be mapped to `readonly_iam_role_arn`"
@@ -42,14 +32,61 @@ variable "readonly_k8s_groups" {
   default     = []
 }
 
+variable "aws_root_account_id" {
+  type        = "string"
+  description = "AWS root account ID"
+}
+
+data "aws_iam_policy_document" "readonly" {
+  statement {
+    actions = [
+      "sts:AssumeRole",
+    ]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${var.aws_root_account_id}:root"]
+    }
+
+    effect = "Allow"
+  }
+}
+
+resource "aws_iam_role" "readonly" {
+  name               = "KubernetesReadOnly"
+  assume_role_policy = "${data.aws_iam_policy_document.readonly.json}"
+  description        = "The Kubernetes readonly role for aws-iam-authenticator"
+}
+
+data "aws_iam_policy_document" "admin" {
+  statement {
+    actions = [
+      "sts:AssumeRole",
+    ]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${var.aws_root_account_id}:root"]
+    }
+
+    effect = "Allow"
+  }
+}
+
+resource "aws_iam_role" "admin" {
+  name               = "KubernetesAdmin"
+  assume_role_policy = "${data.aws_iam_policy_document.admin.json}"
+  description        = "The Kubernetes admin role for aws-iam-authenticator"
+}
+
 module "iam_authenticator_config" {
   source                = "git::https://github.com/cloudposse/terraform-aws-kops-iam-authenticator-config.git?ref=tags/0.1.1"
   cluster_id            = "${var.cluster_id}"
   kube_config_path      = "${var.kube_config_path}"
-  admin_iam_role_arn    = "${var.admin_iam_role_arn}"
+  admin_iam_role_arn    = "${aws_iam_role.admin.arn}"
   admin_k8s_username    = "${var.admin_k8s_username}"
   admin_k8s_groups      = "${var.admin_k8s_groups}"
-  readonly_iam_role_arn = "${var.readonly_iam_role_arn}"
+  readonly_iam_role_arn = "${aws_iam_role.readonly.arn}"
   readonly_k8s_username = "${var.readonly_k8s_username}"
   readonly_k8s_groups   = "${var.readonly_k8s_groups}"
 }
